88d53fed9ea909bd96d0140e772e1183caf679ba
Gavin McFarland <5551+limitlessloop@users.noreply.github.com>
2019-08-01 11:11:32 +0100

    can now access model from inside Template plugin

--- src/lib/template-class.js
+++ src/lib/template-class.js
@@ -1,7 +1,14 @@
+import cloneDeep from 'lodash.clonedeep'
+
+// Import the theme which it's path is specified in the config
+const theme = require(__dirname + '/../' + config.theme).default
+// Create a clone of the theme object which can be modified by the user
+const model = cloneDeep(theme)
+
 export class Template {
 	constructor(name, callback) {
 		this.name = name
-		this.string = callback()
+		this.string = callback(model, theme)
 		this.result = this.render()
 	}
 	render() {
