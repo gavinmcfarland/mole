7d88c7960db0bcd1d8a3d1a206f604fcf6e70434
Gavin McFarland <5551+limitlessloop@users.noreply.github.com>
2019-08-09 02:22:07 +0100

    created first draft of documentation

--- src/lib/theme.js
+++ src/lib/theme.js
@@ -2,6 +2,14 @@ import Config from './Config'
 
 /**
  * Theme data used by templates with outputs
+ *
+ * ```js
+ * // theme/index.js
+ * export default {
+ * 	font: {
+ * 		size: [ 16, 19, 22, 26, 30, 35 ]
+ * 	}
+ * }
  */
 
 class Theme {
@@ -9,7 +17,7 @@ class Theme {
 	/**
 	 * Keeps an original copy of the theme data in case it needs to be referenced by the user
 	 */
-	original() {
+	clone() {
 		/*
 		1. Clone parse theme for use by models and templates */
 	}
