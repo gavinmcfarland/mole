8e8dcfe7637d8cafe2e702fda2f8e1f310205aeb
Gavin McFarland <5551+limitlessloop@users.noreply.github.com>
2019-08-14 14:23:01 +0100

    added comment

--- .babelrc.js
+++ .babelrc.js
@@ -4,6 +4,7 @@ const plugins = [
 	["add-module-exports"],
 ];
 
+// Rewire doesn't seem to be working with add-module-exports, so we use it just for testing
 if (process.env.NODE_ENV === 'test') {
 	plugins.push(["rewire"]);
 }
