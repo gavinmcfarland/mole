279f12a2e97d0c966364797bc2e0a13bf2cfec23
Gavin McFarland <5551+gavinmcfarland@users.noreply.github.com>
2024-08-07 23:32:40 +0100

    Removed dist folder

--- dist/esm/index.js
+++
@@ -1,9 +0,0 @@
-export { m as default } from './index-BWPQsFvS.js';
-import 'fs';
-import 'path';
-import 'events';
-import 'assert';
-import 'util';
-import 'constants';
-import 'stream';
-//# sourceMappingURL=index.js.map
